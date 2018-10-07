97f4bcf5cbcab153d860880e4eb7ff70a0f9bcce
Chang Gong <39935321+MikasaG@users.noreply.github.com>
2018-10-06 21:52:57 -0700

    Add files via upload

--- h20/h20.java
+++ h20/h20.java
@@ -7,97 +7,113 @@ public class h20 {
 	static Semaphore mutex = new Semaphore(1);
 	static Semaphore oxygenQueue = new Semaphore(0);
 	static Semaphore hydrogenQueue = new Semaphore(0);
-	static Semaphore barrier = new Semaphore(3);
-	static int oxygen = 0;
-	static int hydrogen = 0;
-	static int waterId = 1;
-	public static final int water_num = 9;
+	static reusableBarrier Barrier = new reusableBarrier(3);
+	volatile static int oxygen = 0;
+	volatile static int hydrogen = 0;
+	volatile static int waterId = 1;
+	public static final int water_num = 10000;
 
-	static class Oxygen extends Thread {
+	static class Oxygen implements Runnable{
 		@Override
 		public void run()  {
 			try {
 				mutex.acquire();
-			} catch (InterruptedException e){
-				e.printStackTrace();
-			}
-			oxygen++;
-			if (hydrogen >= 2) {
-				hydrogenQueue.release(2);
-				hydrogen -=2;
-				oxygenQueue.release();
-				oxygen--;
-			} else {
-				mutex.release();
-			}
-			try {
+				oxygen++;
+				if (hydrogen >= 2) {
+					hydrogenQueue.release(2);
+					hydrogen -=2;
+					oxygenQueue.release();
+					oxygen--;
+				} else {
+					mutex.release();
+				}
 				oxygenQueue.acquire();
+				bonder.bond(0);
+				Barrier.mywait();
+				mutex.release();
 			} catch (InterruptedException e){
 				e.printStackTrace();
 			}
-			bond();
-			try {
-				barrier.acquire();
-			} catch (InterruptedException e){
-				e.printStackTrace();
-			}
-			mutex.release();
 	    }
 	}
 	
-	static class Hydrogen extends Thread {
+	static class Hydrogen implements Runnable {
 		@Override
 		public void run() {
 			try {
 				mutex.acquire();
-			} catch (InterruptedException e){
-				e.printStackTrace();
-			}
-			hydrogen++;
-			if (hydrogen >= 2 && oxygen >= 1) {
-				hydrogenQueue.release(2);
-				hydrogen -=2;
-				oxygenQueue.release();
-				oxygen--;
-			} else {
-				mutex.release();
-			}
-			try {
+				hydrogen++;
+				if (hydrogen >= 2 && oxygen >= 1) {
+					hydrogenQueue.release(2);
+					hydrogen -=2;
+					oxygenQueue.release();
+					oxygen--;
+				} else {
+					mutex.release();
+				}
 				hydrogenQueue.acquire();
-			} catch (InterruptedException e){
+				bonder.bond(1);
+				Barrier.mywait();
+			}  catch (InterruptedException e){
 				e.printStackTrace();
 			}
-			bond();
-			try {
-				barrier.acquire();
-			} catch (InterruptedException e){
-				e.printStackTrace();
-			}
-			mutex.release();
 	    }
 	}
 	
-	public static synchronized void bond() {
-		System.out.printf("%d Water Bonded\n",waterId);
-		waterId++;
+	static class bonder {//0:Oxygen, 1:Hydrogen
+		static int bonded = 0;
+		static int oxygen_num = 0;
+		static int hydropgen_num = 0;
+		public static synchronized void bond(int flag) {
+			if (flag == 0) {
+				oxygen_num++;
+			} else if (flag == 1){
+				hydropgen_num++;
+			} else {
+				System.out.println("bond error - invalid flag");
+			}
+			if (oxygen_num == 1 && hydropgen_num == 2) {
+				oxygen_num = 0;
+				hydropgen_num = 0;
+				bonded++;
+				System.out.printf("%d Water Bonded\n",bonded);
+			} else if (oxygen_num > 1 || hydropgen_num > 2) {
+				System.out.println("bond error - too many component");
+			}
+		}
 	}
 	public static void main (String[] args) {
+		Oxygen oxygen = new Oxygen();
+		Hydrogen hydrogen = new Hydrogen();
+		Thread[] threads = new Thread[water_num*3];
 		Random rand = new Random();
 		int oxygen_num = 0;
 		int hydrogen_num = 0;
+		ThreadGroup tg = new ThreadGroup("total");
+		long startTime = System.currentTimeMillis(); 
 		for (int i=0; i<water_num*3;i++) {
 			int ran = rand.nextInt(3);
 			if ((ran == 0 && oxygen_num <water_num) || hydrogen_num == water_num*2) {
-				Oxygen oxygenThread = new Oxygen();
+				Thread oxygenThread = new Thread(oxygen);
+				threads[i] = oxygenThread;
 				oxygenThread.start();
 				oxygen_num++;
-				//System.out.printf("%d Oxygen generate\n",oxygen_num);
 			} else {
-				Hydrogen hydrogenThread = new Hydrogen();
+				Thread hydrogenThread = new Thread(hydrogen);
+				threads[i] = hydrogenThread;
 				hydrogenThread.start();
 				hydrogen_num++;
-				//System.out.printf("%d Hydrogen generate\n",hydrogen_num);
 			}
 		}
+		
+		for(int j = 0; j < threads.length; j++){
+			  try {
+				  threads[j].join();
+			  } catch (Exception e) {
+				  e.printStackTrace();
+			  }
+		}
+		long endTime=System.currentTimeMillis(); //get end time
+		System.out.println("Time Cost "+(endTime-startTime)+"ms");
 	}
 }
