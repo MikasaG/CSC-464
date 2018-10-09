3de86d98930ee7946c2b8b45d04959269a022512
Chang Gong <39935321+MikasaG@users.noreply.github.com>
2018-10-08 22:37:43 -0700

    Add files via upload

--- readwrite/readwrite_fair.java
+++ readwrite/readwrite_fair.java
@@ -1,5 +1,6 @@
 package readwrite;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -10,8 +11,10 @@ public class readwrite_fair {
 	static Semaphore mutex = new Semaphore(1);
 	static Semaphore roomEmpty = new Semaphore(1);
 	static int readers = 0;
-	static final int reader_num = 1000;
-	static final int writer_num = 1000;
+	static final int reader_num = 10000;
+	static final int writer_num = 100;
+	static List<Long> readerWaitTime = new ArrayList<Long>(reader_num);
+	static List<Long> writerWaitTime = new ArrayList<Long>(writer_num);
 	
 	private static void read(String s) {
 		System.out.println(s+" is reading");
@@ -31,12 +34,15 @@ public class readwrite_fair {
 		@Override
 		public void run()  {
 			try {
+				long startTime = System.currentTimeMillis(); 
 				mutex.acquire();
 				readers++;
 				if (readers == 1) {
 					roomEmpty.acquire();
 				}
 				mutex.release();
+				long endTime = System.currentTimeMillis(); 
+				readerWaitTime.add(endTime-startTime);
 				read("Reader " + id);
 				
 				mutex.acquire();
@@ -61,8 +67,10 @@ public class readwrite_fair {
 		@Override
 		public void run()  {
 			try {
+				long startTime = System.currentTimeMillis(); 
 				roomEmpty.acquire();
-				
+				long endTime = System.currentTimeMillis(); 
+				writerWaitTime.add(endTime-startTime);
 				write("Writer "+id);
 				
 				roomEmpty.release();
@@ -99,6 +107,27 @@ public class readwrite_fair {
 			  } 
 		}
 		long endTime=System.currentTimeMillis(); //get end time
+		long readerTotal = 0,writerTotal = 0,readerLongest=0,writerLongest=0;
+		for (int i = 0;i<readerWaitTime.size();i++) {
+			long temp = readerWaitTime.get(i);
+			readerTotal+=temp;
+			if (temp > readerLongest) {
+				readerLongest=temp;
+			}
+		}
+		for (int i = 0;i<writerWaitTime.size();i++) {
+			long temp = writerWaitTime.get(i);
+			writerTotal+=temp;
+			if (temp > writerLongest) {
+				writerLongest=temp;
+			}
+		}
+		long readerAverage = readerTotal/readerWaitTime.size();
+		long writerAverage = writerTotal/writerWaitTime.size();
 		System.out.println("Time Cost "+(endTime-startTime)+"ms");	
+		System.out.println("Reader average waiting time "+readerAverage+"ms");	
+		System.out.println("Writder average waiting time "+writerAverage+"ms");	
+		System.out.println("Reader longest waiting time "+readerLongest+"ms");	
+		System.out.println("Writerder longest waiting time "+writerLongest+"ms");	
 	}
 }
--- readwrite/readwrite_nonstarvation.java
+++ readwrite/readwrite_nonstarvation.java
@@ -1,9 +1,13 @@
 package readwrite;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.Semaphore;
+
+import readwrite.readwrite_fair.reader;
+import readwrite.readwrite_fair.writer;
 
 
 public class readwrite_nonstarvation {
@@ -13,6 +17,8 @@ public class readwrite_nonstarvation {
 	static Lightswitch writeswtich = new Lightswitch();
 	static final int reader_num = 10000;
 	static final int writer_num = 100;
+	static List<Long> readerWaitTime = new ArrayList<Long>(reader_num);
+	static List<Long> writerWaitTime = new ArrayList<Long>(writer_num);
 	
 	
 	static class Lightswitch {
@@ -63,10 +69,13 @@ public class readwrite_nonstarvation {
 		@Override
 		public void run()  {
 			try {
+				long startTime = System.currentTimeMillis(); 
 				noreaders.acquire();
 				readswtich.lock(nowriters);
 				noreaders.release();
 				
+				long endTime = System.currentTimeMillis(); 
+				readerWaitTime.add(endTime-startTime);
 				read("Reader "+id);
 				
 				readswtich.unlock(nowriters);
@@ -86,9 +95,13 @@ public class readwrite_nonstarvation {
 		@Override
 		public void run()  {
 			try {
+				long startTime = System.currentTimeMillis(); 
 				writeswtich.lock(noreaders);
 				nowriters.acquire();
 				
+				long endTime = System.currentTimeMillis(); 
+				writerWaitTime.add(endTime-startTime);
+
 				write("Writer "+id);
 				
 				nowriters.release();
@@ -126,6 +139,27 @@ public class readwrite_nonstarvation {
 			  } 
 		}
 		long endTime=System.currentTimeMillis(); //get end time
+		long readerTotal = 0,writerTotal = 0,readerLongest=0,writerLongest=0;
+		for (int i = 0;i<readerWaitTime.size();i++) {
+			long temp = readerWaitTime.get(i);
+			readerTotal+=temp;
+			if (temp > readerLongest) {
+				readerLongest=temp;
+			}
+		}
+		for (int i = 0;i<writerWaitTime.size();i++) {
+			long temp = writerWaitTime.get(i);
+			writerTotal+=temp;
+			if (temp > writerLongest) {
+				writerLongest=temp;
+			}
+		}
+		long readerAverage = readerTotal/readerWaitTime.size();
+		long writerAverage = writerTotal/writerWaitTime.size();
 		System.out.println("Time Cost "+(endTime-startTime)+"ms");	
+		System.out.println("Reader average waiting time "+readerAverage+"ms");	
+		System.out.println("Writder average waiting time "+writerAverage+"ms");	
+		System.out.println("Reader longest waiting time "+readerLongest+"ms");	
+		System.out.println("Writerder longest waiting time "+writerLongest+"ms");	
 	}
 }
